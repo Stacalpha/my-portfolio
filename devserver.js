043cbde05c3d701fd50b158280bcac75d17298d0
Feranmi Akinlade <30551780+uxFeranmi@users.noreply.github.com>
2020-04-27 05:49:04 +0100

    Local dev setup working

--- devserver.js
+++ devserver.js
@@ -1,9 +1,25 @@
 const express = require('express');
+const { exec } = require('child_process');
+
 const app = express();
 
-const PORT = process.env.PORT || 3000;
-const build = './public';
+function start() {
+    const PORT = process.env.PORT || 3000;
+    const build = './public';
+    
+    app.use(express.static(build));
+    
+    app.listen(PORT, ()=>console.log(`Serving ${build} folder on port ${PORT}`));
+}
 
-app.use(express.static(build));
+exec('next build && touch -am functions/index.js', (error, stdout, stderr) => {
+  if (error) {
+    console.error(`exec error: ${error}`);
+    return;
+  }
 
-app.listen(PORT, ()=>console.log(`Serving ${build} folder on port ${PORT}`));
+  console.log(`stdout: ${stdout}`);
+  console.error(`stderr: ${stderr}`);
+
+  start();
+});
--- pages/home.js
+++ pages/home.js
@@ -7,8 +7,8 @@ const Home = ()=> {
       <span></span>
     </div>
 
-    <img id="app-dp" src="/me2.jpg" alt="Me in a blue suit." title="A picture of me in a blue suit." />
-    <small>Web prohyugfy Developer</small>
+    <img id="app-dp" src="/assets/me2.jpg" alt="Me in a blue suit." title="A picture of me in a blue suit." />
+    <small>Web Developer</small>
 
     <h1>
       Hi! I'm Fẹranmi.<br />I build responsive Web Apps.<br />
@@ -16,7 +16,7 @@ const Home = ()=> {
     </h1>
 
     <p>
-      Je travailler with some popular web technologies to creatively bring ideas to live.
+      I work with some popular web technologies to creatively bring ideas to live.
       <small>
         Click any of the icons to &nbsp;
         <a href="https://app.pluralsight.com/profile/feranmi-akinlade" target="_blank" rel="noopener noreferrer">
@@ -32,45 +32,45 @@ const Home = ()=> {
           <span>Angular</span>
         </a>
         <a href="https://app.pluralsight.com/profile/feranmi-akinlade" target="_blank" rel="noopener noreferrer">
-          <img src="/icons/typescript.png" alt="TypeScript.png" title="TypeScript" />
+          <img src="/assets/icons/typescript.png" alt="TypeScript.png" title="TypeScript" />
           <span>TypeScript</span>
         </a>
         <a href="https://app.pluralsight.com/profile/feranmi-akinlade" target="_blank" rel="noopener noreferrer">
-          <img src="/icons/sass.png" alt="Sass Logo" title="Less" />
+          <img src="/assets/icons/sass.png" alt="Sass Logo" title="Less" />
           <span>Less</span>
         </a>
       </div>
 
       <div>
         <a href="https://app.pluralsight.com/profile/feranmi-akinlade" target="_blank" rel="noopener noreferrer">
-          <img src="/icons/logo.svg" alt="React Logo" title="React" />
+          <img src="/assets/icons/logo.svg" alt="React Logo" title="React" />
           <span>React</span>
         </a>
         <a href="https://app.pluralsight.com/profile/feranmi-akinlade" target="_blank" rel="noopener noreferrer">
-          <img src="/icons/javascript.png" alt="JavaScript Logo" title="JavaScript" />
+          <img src="/assets/icons/javascript.png" alt="JavaScript Logo" title="JavaScript" />
           <span>JavaScript</span>
         </a>
         <a href="https://app.pluralsight.com/profile/feranmi-akinlade" target="_blank" rel="noopener noreferrer">
-          <img src="/icons/css3.png" alt="CSS3 Logo" title="CSS 3" />
+          <img src="/assets/icons/css3.png" alt="CSS3 Logo" title="CSS 3" />
           <span>CSS</span>
         </a>
         <a href="https://app.pluralsight.com/profile/feranmi-akinlade" target="_blank" rel="noopener noreferrer">
-          <img src="/icons/html5.png" alt="HTML5 Logo" title="HTML 5" />
+          <img src="/assets/icons/html5.png" alt="HTML5 Logo" title="HTML 5" />
           <span>HTML</span>
         </a>
       </div>
 
       <div>
         <a href="https://app.pluralsight.com/profile/feranmi-akinlade" target="_blank" rel="noopener noreferrer">
-          <img src="/icons/electron.png" alt="Electron.js Logo" title="Electron.js" />
+          <img src="/assets/icons/electron.png" alt="Electron.js Logo" title="Electron.js" />
           <span>Electron.js</span>
         </a>
         <a href="https://app.pluralsight.com/profile/feranmi-akinlade" target="_blank" rel="noopener noreferrer">
-          <img src="/icons/nodejs.png" alt="Node.js Logo" title="Node.js" />
+          <img src="/assets/icons/nodejs.png" alt="Node.js Logo" title="Node.js" />
           <span>Node.js</span>
         </a>
         <a href="https://app.pluralsight.com/profile/feranmi-akinlade" target="_blank" rel="noopener noreferrer">
-          <img src="/icons/expressjs.png" alt="Express.js Logo" title="Express.js" />
+          <img src="/assets/icons/expressjs.png" alt="Express.js Logo" title="Express.js" />
           <span>Express.js</span>
         </a>
       </div>
@@ -79,27 +79,27 @@ const Home = ()=> {
     <address>
       <div id="contact-infos">
         <a href="tel:+2348179925186" id="whatsapp-contact">
-          <img src="/icons/whatsapp.png" alt="Contact me on WhatsApp" />
+          <img src="/assets/icons/whatsapp.png" alt="Contact me on WhatsApp" />
         </a>
         <a href="tel:+2348179925186" id="phone-contact">
-          <img src="/icons/gsm.png" alt="Contact me via SMS" />
+          <img src="/assets/icons/gsm.png" alt="Contact me via SMS" />
         </a>
         <a href="mailto:feranmiakinlade@gmail.com" id="email-contact">
-          <img src="/icons/email.png" alt="Send me an email" />
+          <img src="/assets/icons/email.png" alt="Send me an email" />
         </a>
       </div>
       <div id="profile-links">
         <a href="https://github.com/stacalpha" id="github-profile"
             target="_blank" rel="noopener noreferrer">
-          <img src="/icons/github.png" alt="Check out my GitHub profile" />
+          <img src="/assets/icons/github.png" alt="Check out my GitHub profile" />
         </a>
         <a href="https://stackoverflow.com/users/story/7665742" target="_blank"
             rel="noopener noreferrer" id="stackoverflow-dev-story">
-          <img src="/icons/stackoverflow.png" alt="My developer story - Stack Overflow" />
+          <img src="/assets/icons/stackoverflow.png" alt="My developer story - Stack Overflow" />
         </a>
         <a href="https://www.linkedin.com/in/akinlade-feranmi-0b6bbb170"
             target="_blank" rel="noopener noreferrer" id="linkedin-profile">
-          <img src="/icons/linkedin.png" alt="View my LinkedIn profile" />
+          <img src="/assets/icons/linkedin.png" alt="View my LinkedIn profile" />
         </a>
       </div>
     </address>
